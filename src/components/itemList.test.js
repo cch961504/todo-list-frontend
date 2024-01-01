57825a5eb06b390bd86e1bfa5413bb2fb4b2df18
Cheuk-Hong Cheng <cch961504@gmail.com>
2024-01-01 13:55:04 +0000

    unit test for toggleItem

--- src/components/itemList.test.js
+++ src/components/itemList.test.js
@@ -1,9 +1,18 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import ItemList from "./ItemList"; // Adjust the import path as necessary
 
 describe("ItemList component", () => {
+  const exampleItemList = [
+    {
+      id: 1,
+      content: "Existing Item 1",
+      checked: 0,
+      owner: "owner",
+      modified: 0,
+    },
+  ];
   test("adds a new item to the list", async () => {
     render(<ItemList />);
     const inputField = screen.getByRole("textbox");
@@ -17,20 +26,23 @@ describe("ItemList component", () => {
   });
 
   test("populate list", async () => {
-    localStorage.setItem(
-      "items",
-      JSON.stringify([
-        {
-          id: 1,
-          content: "Existing Item 1",
-          checked: 0,
-          owner: "owner",
-          modified: 0,
-        },
-      ]),
-    );
+    localStorage.setItem("items", JSON.stringify(exampleItemList));
     render(<ItemList />);
 
     expect(screen.getByText("Existing Item 1")).toBeInTheDocument();
+  });
+
+  test("toggles the checkbox of an item", async () => {
+    localStorage.setItem("items", JSON.stringify(exampleItemList));
+    render(<ItemList />);
+    const checkbox = screen.getByLabelText("Existing Item 1");
+
+    // Toggle the checkbox
+    fireEvent.click(checkbox);
+    await waitFor(() => expect(checkbox).toBeChecked());
+    
+    // Toggle the checkbox
+    fireEvent.click(checkbox);
+    await waitFor(() => expect(checkbox).not.toBeChecked())
   });
 });
